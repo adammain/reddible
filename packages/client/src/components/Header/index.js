d4b9bd26b15dc3d5ad68cefb2594b8bd0b0bd9cd
amtruenorth <northforth@me.com>
2017-12-23 12:39:42 -0500

    feat: Add header component

--- packages/client/src/components/Header/index.js
+++ packages/client/src/components/Header/index.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import { fetchCategories, selectCategory } from '../../actions/categories';
 
 class Header extends Component {
 
+  componentDidMount() {
+    this.props.fetchCategories();
+  }
+
   render () {
+    const { categories } = this.props.categories;
+    const routeCategory = this.props.match.params.category;
     return (
-      <h1>Connected</h1>
-    )
+      <nav>
+        <Link 
+          to='/'
+          alt='Readable'
+        >Readable</Link>
+        <div>
+          <ul>
+            { categories !== undefined && categories.map( category => (
+              <li key={category.path} className={ 
+                "nav-item" + (routeCategory === category.name ? " active" : "")
+              }>
+                <Link
+                  to={`/${category.path}`}
+                >{category.name}</Link>
+              </li>
+            ))}
+            <li>
+              <Link
+                to='/new'
+              >new post</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header
+const mapStateToProps  = ({ categories, selectedCategory }) => ({
+  categories,
+  selectedCategory
+})
+
+export default connect(mapStateToProps, { fetchCategories, selectCategory })(Header);

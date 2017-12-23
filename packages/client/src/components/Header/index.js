6fc3aec2ac77277e5b16575f31de27b185ecd368
amtruenorth <northforth@me.com>
2017-12-23 10:50:55 -0500

    chore: Add redux packages and initial setup

---
+++ packages/client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+
+class Header extends Component {
+
+  render () {
+    return (
+      <h1>Connected</h1>
+    )
+  }
+}
+
+export default Header

97a33fc3db52442ad67e97f1d171dc7035014dc9
amtruenorth <northforth@me.com>
2017-12-23 12:33:49 -0500

    chore: Include redux-thunk package for async calls

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-    reducers
+    reducers,
+    composeEnhancers(applyMiddleware(thunk))
 );
   
 ReactDOM.render(

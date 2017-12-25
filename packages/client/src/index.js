1e8152ea433d6bd159c764271aae500199ced335
amtruenorth <northforth@me.com>
2017-12-25 12:42:27 -0500

    refactor: Remove redux and router packages from index

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,28 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import './index.css';
-import App from './App';
-import reducers from './reducers';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import './index.css'
+import App from './App'
 
-const store = createStore(
-    reducers,
-    composeEnhancers(applyMiddleware(thunk))
-);
+import { store } from './store';
+import registerServiceWorker from './registerServiceWorker'
   
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>, 
-    document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+)
 
-registerServiceWorker();
+registerServiceWorker()

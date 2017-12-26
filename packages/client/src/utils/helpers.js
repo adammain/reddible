8db662b2b5ab7d880c9581d85cda144c3d18b3d5
amtruenorth <northforth@me.com>
2017-12-26 13:04:44 -0500

    refactor: Sync remote helper file and add new Header file

--- packages/client/src/utils/helpers.js
+++ packages/client/src/utils/helpers.js
@@ -1,6 +1,6 @@
 import Moment from 'moment'
 
-// Convert timestamp to date format 
+// Convert timestamp to date formats 
 export const fromNow = (time) => {
   return Moment(time).valueOf() <= Moment().subtract(7, 'days').valueOf() 
     ? Moment(time).format('LLL') 
